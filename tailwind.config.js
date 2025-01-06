59ae1c3e55551826ad05590af8e096cb179c49bd
Pedro Botega <botegapedro@gmail.com>
2025-01-06 18:48:17 +0000

    updates tailwind content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  // The "content" option tells Tailwind which files to scan for classes.
+  // In this case, we're telling it to scan all files in the "app" and
+  // "components" directories, as well as all files in the "pages" directory.
+  // This allows us to use Tailwind classes in our React components.
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
