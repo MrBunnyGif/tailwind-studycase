ffb8abaa482843ad92cd6d7618b8fddc989396d4
Pedro Botega <botegapedro@gmail.com>
2025-01-07 09:50:56 +0000

    background

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <h1 className="text-green-600">
+      <h1 className="bg-green-600 text-white">
         Hello world!
       </h1>
       <h2 className="text-red-500">
