fc6cb0e6feb7c6f8c659391741106ccc0b971cea
Pedro Botega <botegapedro@gmail.com>
2025-01-07 15:21:05 +0000

    gap

--- app/page.tsx
+++ app/page.tsx
@@ -4,63 +4,16 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <div className="flex min-h-screen">
+      <div className="flex min-h-screen gap-4">
         <div className="bg-red-400 w-1/5">
           <p>oi</p>
         </div>
         <div className="bg-blue-400 flex-auto">
           <p>asdasddsa</p>
-          <br />
-          <p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
-          <br /><p>asdasddsa</p>
           <br /><p>asdasddsa</p>
           <br /><p>asdasddsa</p>
           <br /><p>asdasddsa</p>
           <br />
-
         </div>
       </div>
     </>
