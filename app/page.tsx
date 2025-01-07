5776054955796a7d25acd1cd146c195ca8cdf700
Pedro Botega <botegapedro@gmail.com>
2025-01-07 14:31:18 +0000

    margin

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <div className="font-bold text-white border pl-1"> hello</div>
-      <div className="font-bold text-white border pt-1"> hello</div>
-      <div className="font-bold text-white border px-1"> hello</div>
-      <div className="font-bold text-white border py-1"> hello</div>
+      <div className="font-bold text-white border pl-1 ml-1"> hello</div>
+      <div className="font-bold text-white border pt-1 mt-1"> hello</div>
+      <div className="font-bold text-white border px-1 mx-1"> hello</div>
+      <div className="font-bold text-white border py-1 my-1"> hello</div>
     </>
   );
 }
