6e993e38bc85f2e2e78d80d3eb0917ccf60d6ca9
Pedro Botega <botegapedro@gmail.com>
2025-01-07 15:16:29 +0000

    height

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,64 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <div className="flex h-6">
-        <div className="bg-red-400 w-1/2"></div>
-        <div className="bg-blue-400 w-1/2"></div>
-      </div>
-      <div className="flex h-6">
-        <div className="bg-blue-400 w-1/3"></div>
-        <div className="bg-red-400 w-2/3"></div>
-      </div>
-      <div className="flex h-6">
-        <div className="bg-red-400 w-6"></div>
-        <div className="bg-blue-400 flex-auto"></div>
+      <div className="flex min-h-screen">
+        <div className="bg-red-400 w-1/5">
+          <p>oi</p>
+        </div>
+        <div className="bg-blue-400 flex-auto">
+          <p>asdasddsa</p>
+          <br />
+          <p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br /><p>asdasddsa</p>
+          <br />
+
+        </div>
       </div>
     </>
   );
