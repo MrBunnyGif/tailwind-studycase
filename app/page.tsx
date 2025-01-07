cd785247bd8413fc31553714d857d0eafa8eb01b
Pedro Botega <botegapedro@gmail.com>
2025-01-07 14:53:54 +0000

    negative margin

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <div className="font-bold text-white border pl-1 ml-1"> hello</div>
-      <div className="font-bold text-white border pt-1 mt-1"> hello</div>
-      <div className="font-bold text-white border px-1 mx-1"> hello</div>
-      <div className="font-bold text-white border py-1 my-1"> hello</div>
+      <h1 className="text-2xl font-bold bg-red-400">title</h1>
+      <p className="border -mt-[10px]">this is the paragraph here</p>
     </>
   );
 }
