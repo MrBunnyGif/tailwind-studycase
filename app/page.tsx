335ee0d8063104f6523187a0183acd05275924eb
Pedro Botega <botegapedro@gmail.com>
2025-01-07 14:12:33 +0000

    text transform

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
       <h1 className="text-4xl italic">qwerty</h1>
       <h1 className="text-2xl underline decoration-wavy underline-offset-8 leading-loose">qwerty</h1>
       <h1 className="text-xl line-through decoration-double decoration-red-600 leading-normal">qwerty</h1>
-      <h2 className="text-base overline decoration-dotted decoration-4">asdf</h2>
-      <h3 className="text-sm">zxcvb</h3>
-      <p className="text-xs">paragraph here</p>
+      <h2 className="text-base overline decoration-dotted decoration-4 uppercase">as df</h2>
+      <h3 className="text-sm lowercase">ZXcvb</h3>
+      <p className="text-xs capitalize">paragraph here</p>
     </>
   );
 }
