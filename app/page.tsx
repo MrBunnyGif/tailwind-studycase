070ec85787b1064e43d9fcc8ed46c15599cb1b8f
Pedro Botega <botegapedro@gmail.com>
2025-01-07 14:10:47 +0000

    line height (leading)

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <>
       <h1 className="text-4xl italic">qwerty</h1>
-      <h1 className="text-2xl underline decoration-wavy underline-offset-8">qwerty</h1>
-      <h1 className="text-xl line-through decoration-double decoration-red-600">qwerty</h1>
+      <h1 className="text-2xl underline decoration-wavy underline-offset-8 leading-loose">qwerty</h1>
+      <h1 className="text-xl line-through decoration-double decoration-red-600 leading-normal">qwerty</h1>
       <h2 className="text-base overline decoration-dotted decoration-4">asdf</h2>
       <h3 className="text-sm">zxcvb</h3>
       <p className="text-xs">paragraph here</p>
