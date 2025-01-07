419b7ebfb44bb3b5f6274f1583fac4f46d548478
Pedro Botega <botegapedro@gmail.com>
2025-01-07 14:06:18 +0000

    text decoration

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <h1 className="text-4xl">qwerty</h1>
-      <h1 className="text-2xl">qwerty</h1>
-      <h1 className="text-xl">qwerty</h1>
-      <h2 className="text-base">asdf</h2>
+      <h1 className="text-4xl italic">qwerty</h1>
+      <h1 className="text-2xl underline decoration-wavy underline-offset-8">qwerty</h1>
+      <h1 className="text-xl line-through decoration-double decoration-red-600">qwerty</h1>
+      <h2 className="text-base overline decoration-dotted decoration-4">asdf</h2>
       <h3 className="text-sm">zxcvb</h3>
       <p className="text-xs">paragraph here</p>
     </>
