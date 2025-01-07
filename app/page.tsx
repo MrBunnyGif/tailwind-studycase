6a1a7289835014ca7808dae485351553af07aa3c
Pedro Botega <botegapedro@gmail.com>
2025-01-07 15:07:28 +0000

    width

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,18 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl font-bold bg-red-400">title</h1>
-      <p className="border -mt-[10px]">this is the paragraph here</p>
+      <div className="flex h-6">
+        <div className="bg-red-400 w-1/2"></div>
+        <div className="bg-blue-400 w-1/2"></div>
+      </div>
+      <div className="flex h-6">
+        <div className="bg-blue-400 w-1/3"></div>
+        <div className="bg-red-400 w-2/3"></div>
+      </div>
+      <div className="flex h-6">
+        <div className="bg-red-400 w-6"></div>
+        <div className="bg-blue-400 flex-auto"></div>
+      </div>
     </>
   );
 }
