570b9b7d66cd6bd5c59d923b9c92b0938917aec2
Pedro Botega <botegapedro@gmail.com>
2025-01-08 15:18:37 +0000

    flex

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,18 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
-      <h1 className="mb-4">h1</h1>
-      <h2 className="mb-3">h2</h2>
-      <h3>h3</h3>
-    </>
+    <div className="Parent">
+      <div>Header</div>
+
+      <div className="flex flex-row-reverse">
+        <div>
+          Main content
+        </div>
+        <div className="flex flex-col">
+          <div>Sidebar</div>
+          <div>menu</div>
+        </div>
+      </div>
+    </div>
   );
 }
