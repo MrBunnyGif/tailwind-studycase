5e79bf397e38d686d738ac0d9bca4983306808b3
Pedro Botega <botegapedro@gmail.com>
2025-01-08 16:32:20 +0000

    Container

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,9 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-4 border gap-1 *:bg-orange-600">
-      <div className="border">div 1</div>
-      <div className="border col-span-3">div 2</div>
-      <div className="border col-span-2">div 3</div>
-      <div className="border col-span-3 hover:bg-red-600 max-[320px]:text-center">div 4</div>
-      <div className="border">div 5</div>
-    </div>
+    <div className="container bg-red-500 mx-auto" >
+      {/* The "container" class in Tailwind CSS centers the content and applies responsive padding according to the screen size */}
+      <h1>Hello</h1>
+    </div >
   );
 }
