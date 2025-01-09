205fc00567a3269f50f671ce07daaa3e03226c82
Pedro Botega <botegapedro@gmail.com>
2025-01-09 15:07:42 +0000

    hover, transition, delay, scale

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div className="container space-y-1" >
       {/* This shadow will NOT be red at medium screens and above */}
-      <div className="bg-slate-600 aspect-video shadow-lg shadow-red-500 md:shadow-xl"></div>
+      <div className="bg-slate-600 aspect-video shadow-lg shadow-red-500 md:shadow-xl hover:scale-105 transition delay-100"></div>
       {/* This shadow WILL be red at medium screens and above */}
-      <div className="bg-red-400 aspect-square shadow-lg shadow-red-500 md:shadow-xl md:shadow-red-500"></div>
+      <div className="bg-red-400 aspect-square shadow-lg shadow-red-500 md:shadow-xl md:shadow-red-500 hover:-translate-y-1/3 transition delay-100"></div>
     </div >
   );
 }
