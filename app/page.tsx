ee05179436a8cb7802e707461e06bb0a3e1a3899
Pedro Botega <botegapedro@gmail.com>
2025-01-09 15:11:37 +0000

    animations

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,15 @@ export default function Home() {
       {/* This shadow will NOT be red at medium screens and above */}
       <div className="bg-slate-600 aspect-video shadow-lg shadow-red-500 md:shadow-xl hover:scale-105 transition delay-100"></div>
       {/* This shadow WILL be red at medium screens and above */}
-      <div className="bg-red-400 aspect-square shadow-lg shadow-red-500 md:shadow-xl md:shadow-red-500 hover:-translate-y-1/3 transition delay-100"></div>
+      <div className="bg-red-400 aspect-square shadow-lg shadow-red-500 md:shadow-xl md:shadow-red-500 hover:-translate-y-1/3 transition delay-100">
+        <button className="animate-spin"> spin</button>
+        <br />
+        <button className="animate-ping"> ping</button>
+        <br />
+        <button className="animate-pulse"> pulse</button>
+        <br />
+        <button className="animate-bounce"> bounce</button>
+      </div>
     </div >
   );
 }
