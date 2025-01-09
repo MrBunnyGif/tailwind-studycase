51b6831848c8b9fd41ee2f3d96fe7e9018ffe64c
Pedro Botega <botegapedro@gmail.com>
2025-01-09 15:16:55 +0000

    skew

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,16 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className="container space-y-1" >
-      {/* This shadow will NOT be red at medium screens and above */}
-      <div className="bg-slate-600 aspect-video shadow-lg shadow-red-500 md:shadow-xl hover:scale-105 transition delay-100"></div>
-      {/* This shadow WILL be red at medium screens and above */}
-      <div className="bg-red-400 aspect-square shadow-lg shadow-red-500 md:shadow-xl md:shadow-red-500 hover:-translate-y-1/3 transition delay-100">
-        <button className="animate-spin"> spin</button>
+    <div className="container space-y-1 skew-x-12 border" >
+      <p className="text-lg">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet nulla auctor, vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam quis risus eget velit adipiscing euismod. Sed vitae justo in nunc convallis porta. Nulla facilisi.
         <br />
-        <button className="animate-ping"> ping</button>
         <br />
-        <button className="animate-pulse"> pulse</button>
+        In vel lectus ac justo ultrices varius. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam quis risus eget velit adipiscing euismod. Sed vitae justo in nunc convallis porta. Nulla facilisi.
         <br />
-        <button className="animate-bounce"> bounce</button>
-      </div>
+        <br />
+        Nullam quis risus eget velit adipiscing euismod. Sed vitae justo in nunc convallis porta. Nulla facilisi. In vel lectus ac justo ultrices varius. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </p>
     </div >
   );
 }
