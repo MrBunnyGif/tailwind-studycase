4424da7bd665961e9f47ae17607a7b43ecb22872
Pedro Botega <botegapedro@gmail.com>
2025-01-09 14:53:24 +0000

    shadow

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className="container space-y-1" >
-      <div className="bg-slate-600 aspect-video"></div>
-      <div className="bg-red-400 aspect-square"></div>
+      {/* This shadow will NOT be red at medium screens and above */}
+      <div className="bg-slate-600 aspect-video shadow-lg shadow-red-500 md:shadow-xl"></div>
+      {/* This shadow WILL be red at medium screens and above */}
+      <div className="bg-red-400 aspect-square shadow-lg shadow-red-500 md:shadow-xl md:shadow-red-500"></div>
     </div >
   );
 }
